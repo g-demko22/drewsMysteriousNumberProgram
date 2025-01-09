14ec381a5d46bb710d6a182530a85819b90f31d9
Garrett Demko <185560604+g-demko22@users.noreply.github.com>
2025-01-09 12:57:59 -0500

    Add event listener

--- script.js
+++ script.js
@@ -2,10 +2,37 @@ const conversionNums = [
     [5, 3],
     [10, 6],
     [15, 3],
-    [25, -2]
+    [25, -2],
     [30, 2],
     [40, 2],
     [50, 2],
     [70, -5],
     [90, 5]
 ]
+
+const submitButton = document.getElementById('submit')
+
+const calculateResponse = event => {
+    const responseArr = [];
+    for(let i = 1; i < 11; i++) {
+        responseArr.push(Number(document.getElementById('num' + i.toString()).value))
+    }
+    responseArr.forEach((responseNum, responseIndex) => {
+        let changed = false;
+        conversionNums.forEach(conversionNum => {
+            if(responseNum === conversionNum[0]) {
+                responseArr.splice(responseIndex, 1, conversionNum[1])
+                changed = true;
+            }
+        })
+        if(!changed) {
+            responseArr.splice(responseIndex, 1, 0)
+        }
+    })
+
+    const total = responseArr.reduce((accumulator, currentValue) => accumulator + currentValue);
+    
+    document.getElementById('total').innerText = 'Total ' + total;
+}
+
+submitButton.addEventListener('click', calculateResponse)
